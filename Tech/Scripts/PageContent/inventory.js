8e1d53f5871744da0b9c75031fe6fb18fb85202e
RJ Salvador <rjsalvadorr@gmail.com>
2018-03-25 21:26:00 -0400

    #8 - Styling the table and table rows

--- Tech/Scripts/PageContent/inventory.js
+++ Tech/Scripts/PageContent/inventory.js
@@ -1,9 +1,17 @@
 
+function colourRows() {
+  $('#userdata tr td').each(function() {
+    if ($(this).text() === 'MIDI' || $(this).text() === 'MID') {
+        $(this).closest('tr').css('background-color', '#570009');
+    } else if ($(this).text() === 'WAV') {
+      $(this).closest('tr').css('background-color', '#0A0A0A');
+    }
+  });
+}
+
 function load() {
-            
   data.forEach(function(file) {
-      var tblRow = "<tr>" + "<td>" + file.projectId + "</td>" +
-      "<td>" + file.name + "</td>" + "<td>" + file.tempo + "</td>" + "<td>" + file.version + "</td>" + "<td>" + file.type.toUpperCase() + "</td>" + "</tr>"
+      var tblRow = "<tr><td>" + file.projectId + "</td><td>" + file.name + "</td><td>" + file.tempo + "</td><td>" + file.version + "</td><td>" + file.type.toUpperCase() + "</td></tr>";
       $(tblRow).appendTo("#userdata tbody");
   });
 
@@ -13,5 +21,10 @@ function load() {
       perPageOptions: [1000,1200,1500,1750]
     }
   });
+  var normalTable = $('#userdata');
+  normalTable.bind('dynatable:afterUpdate', function(e, dynatable){
+    colourRows();
+  });
+
   var dTable = $('#userdata').dynatable();
 }
