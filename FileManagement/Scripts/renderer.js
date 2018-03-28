62ffa4b7ec22a9f80e37dda5cf1152516169ce22
RJ Salvador <randolph.salvador@gmail.com>
2018-03-28 14:53:59 -0400

    #15 - Started the basic GUI

---
+++ FileManagement/Scripts/renderer.js
@@ -0,0 +1,37 @@
+
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+var Utils = require('./tools/utils');
+
+var outputTextarea = document.querySelector('#output');
+
+function bindClickFunction(selector, func) {
+  document.querySelector(selector).addEventListener('click', func);
+}
+
+function handleNewJamFolder() {
+  var outVal = 'handleNewJamFolder()';
+  outputTextarea.innerHTML = outVal;
+}
+
+function handleNewProjectFolder() {
+  var outVal = 'handleNewProjectFolder()';
+  outputTextarea.innerHTML = outVal;
+}
+
+function handleRemoveEmptyProjects() {
+  var outVal = 'handleRemoveEmptyProjects()';
+  outputTextarea.innerHTML = outVal;
+}
+
+function handleUpdateInventory() {
+  var outVal = 'handleUpdateInventory()';
+  outputTextarea.innerHTML = outVal;
+}
+
+bindClickFunction('#btnNewJamFolder', handleNewJamFolder);
+bindClickFunction('#btnNewProjectFolder', handleNewProjectFolder);
+bindClickFunction('#btnRemoveEmptyProjects', handleRemoveEmptyProjects);
+bindClickFunction('#btnUpdateInventory', handleUpdateInventory);
